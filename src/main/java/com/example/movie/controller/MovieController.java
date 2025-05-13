99bebec6b1cc92fa02c042e36cfacf381b7b4e33
shin seung woo <ssw030209@gmail.com>
2025-05-13 18:06:47 +0900

    first commit

---
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -0,0 +1,18 @@
+package com.example.movie.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log4j2
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+
+    @GetMapping("/list")
+    public void getHome() {
+    }
+
+}
